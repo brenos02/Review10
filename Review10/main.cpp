4b3e25eb7de0608ad4295147d6481d4c6dd018d9
Breno Sallouti <brenosallouti@icloud.com>
2017-06-09 16:55:56 -0300

    Finished

--- Review10/main.cpp
+++ Review10/main.cpp
@@ -4,12 +4,16 @@
 //
 //  Created by Breno Sallouti on 09/06/17.
 //  Copyright © 2017 Breno Sallouti. All rights reserved.
-//
+//Get input from user in inches and output in centimeters
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    float a;
+    float b;
+    b= 2.54;
+    std::cout<<"Please input a value in inches: \n";
+    std::cin>> a;
+    std::cout<< "That value in centimeters is" << a*b;
     return 0;
 }
